d5393d926b86e631e8bb45c0ffbaf62faeb7207f
Shubham kumar kushwaha <114673387+Quadwinner@users.noreply.github.com>
2024-04-29 14:18:33 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -27,8 +27,7 @@ const Services = () => (
           continue to improve
         </h1>
         <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-          The best choice for buying and selling your crypto assets, with the
-          various super friendly services we offer
+        
         </p>
       </div>
 
